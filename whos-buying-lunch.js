42ed5d745cd4208a5c505e70e449f9092aa26ce3
Alex Bourlotos <alex.bourlotos@wnins.com>
2022-05-06 14:40:26 -0500

    133-whos-buying-lunch-challenge complete.

---
+++ whos-buying-lunch.js
@@ -0,0 +1,10 @@
+function whosPaying(names) {
+    
+/******Don't change the code above*******/
+    
+    //Write your code here.
+    var number = Math.random();
+    number = number * names.length;
+    return(names[Math.floor(number)] + " is going to buy lunch today!");
+/******Don't change the code below*******/    
+}
